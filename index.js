48c29c92ff84d1b98eccfb8aa8965b1a1d4b108a
jzolo22 <jzolotarev@gmail.com>
2020-11-29 17:10:04 -0500

    commit tree d6fa3e2d00b66a9563104e29307ee099ca43f4b2

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your code here
+// class Dog {
+//     constructor(name, breed) {
+//         this.name = name
+//         this.breed = breed
+//     }
+// }
+
+// let fluffy = new Dog("Fluffy", "3-headed")
+
+class Breakfast {
+    constructor(food, drink) {
+        this.food = food 
+        this.drink = drink
+    }
+}
+
+class Lunch {
+    constructor(salad, soup, drink) {
+        this.salad = salad 
+        this.soup = soup 
+        this.drink = drink
+    }
+}
+
+class Dinner {
+    constructor(salad, soup, entree, dessert) {
+        this.salad = salad 
+        this.soup = soup 
+        this.entree = entree 
+        this._dessert = dessert
+    }
+}
